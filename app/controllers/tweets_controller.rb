b2cca9f0243f4d20ff075f355017f8a09d33632d
井田 献一朗 <ida.kenichiro@gmail.com>
2025-03-06 23:26:07 +0900

    Pass user name by form

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,19 @@
 class TweetsController < ApplicationController
+  before_action :set_user
+
   def index
     @tweets = Tweet.includes(:user).order(created_at: :desc)
-    @alice = User.find_by(name: "Alice")  # Aliceのアカウント取得
     @new_tweet = Tweet.new
   end
 
   def create
-    alice = User.find_by(name: "Alice")
-    Tweet.create!(content: params[:tweet][:content], user: alice)
+    Tweet.create!(content: params[:tweet][:content], user: @user)
     redirect_to root_path, notice: "ツイートしました！"
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(name: params[:name])
   end
 end
